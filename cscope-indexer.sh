7efa174dcb3489f18b8060a47bbff6930791fb76
Himanshu Mehra <himanshu.mehra@ericsson.com>
2015-02-04 12:23:36 -0800

    General Update

--- cscope-indexer.sh
+++ cscope-indexer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIR=$HOME/prototype-project
+DIR=/project/swbuild169/emehhim/spider
 cd $DIR
    
 # Delete any existing cscope files
@@ -10,14 +10,15 @@ rm -rf cscope.out cscope.in.out cscope.po.out tags TAGS
 #Find files to index
 echo "Creating index ..."
 find $DIR -name "*.c" -o -name "*.cpp" -o -name "*.h"   \
-    -o -name "*.hpp" -o -name "*.yang" -o -name "*.xml" -o -name "*.proto" \
-    -o -name "*.mib" | grep -v "dse" > cscope.files
+    -o -name "*.hpp"  -o -name "*.xml"  \
+    -o -name "*.py" -o -name "*.json" | grep -v "dse" > cscope.files
 
 cscope -R -b -q -i cscope.files
+rm cscope.files
 echo "Indexing files ... done"
 
 # Create Tags
 echo "Creating tags ..."
-(cd $DIR && find . -type f -iname "*.[ch]" | xargs etags -a)
+(cd $DIR && find . -type f -iname "*.[ch]" -o -name "*.py" | xargs etags -a)
 echo "Creating tags ... done"
 exit 0
--- place-files.sh
+++ place-files.sh
@@ -2,12 +2,11 @@
 
 # remove existing files
 rm $HOME/.emacs $HOME/.tmux.conf
-(cd $HOME && rm -rf .emacsbackup)
 
 # Add files to respective locations
 cp emacs $HOME/.emacs
 cp tmux.conf $HOME/.tmux.conf
-cp -r myemacs/ $HOME/.myemacs
+cp -r myemacs $HOME/.myemacs
 
 # make temp directory for emacs backup
 (cd $HOME && mkdir .emacsbackup)
