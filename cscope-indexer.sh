e85ee532ee3de00b7aaf925c3c1da2ced452c187
Himanshu Mehra <himmehra@cisco.com>
2015-08-26 15:41:31 -0700

    Update etags in indexer

--- cscope-indexer.sh
+++ cscope-indexer.sh
@@ -2,23 +2,43 @@
 
 DIR=$HOME
 cd $DIR
-   
+
 # Delete any existing cscope files
-echo "Deleting existing cscope.out cscope.in.out cscope.po.out TAGS"
+echo "Deleting existing cscope.out cscope.in.out cscope.po.out"
 rm -rf cscope.out cscope.in.out cscope.po.out tags TAGS
 
-#Find files to index
+# Find files to index
 echo "Creating index ..."
-find $DIR -name "*.c" -o -name "*.cpp" -o -name "*.h"   \
-    -o -name "*.hpp"  -o -name "*.xml"  \
-    -o -name "*.py" -o -name "*.json" | grep -v "dse" > cscope.files
 
+find . -name "*.c"    \
+    -o -name "*.cpp"  \
+    -o -name "*.h"    \
+    -o -name "*.cc"   \
+    -o -name "*.hpp"  \
+    -o -name "*.xml"  \
+    -o -name "*.py"   \
+    -o -name "*.json" > cscope.files
+
+# Run cscope
 cscope -R -b -q -i cscope.files
-rm cscope.files
+
 echo "Indexing files ... done"
 
-# Create Tags
+# Generate TAGS
 echo "Creating tags ..."
-(cd $DIR && find . -type f -iname "*.[ch]" -o -name "*.py" | xargs etags -a)
+etags $(find $PWD -name "*.c"   -print \
+    -o -name "*.cpp" -print \
+    -o -name "*.h"   -print \
+    -o -name "*.cc"  -print \
+    -o -name "*.hpp" -print \
+    -o -name "*.xml" -print \
+    -o -name "*.py"  -print \
+    -o -name "*.json" -print)
+
 echo "Creating tags ... done"
+
+# Clean up
+rm cscope.files
+
+cd -
 exit 0
