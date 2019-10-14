eb7796245ce2f72ce26f791087291c9e96a13d31
Himanshu Mehra <hmehra@cohesity.com>
2019-10-13 20:46:40 -0700

    Added custom bashrc

--- place-files.sh
+++ place-files.sh
@@ -3,17 +3,19 @@
 # remove existing files
 rm -rf $HOME/.emacs       \
        $HOME/.tmux.conf   \
-       $HOME/.myemacs     \
        $HOME/.emacsbackup
 
 # Add files to respective locations
-mkdir -p $HOME/.emacs.d
-cp emacs $HOME/.emacs
-cp myinit.el $HOME/.emacs.d
-cp myinit.org $HOME/.emacs.d
-cp tmux.conf $HOME/.tmux.conf
-cp gitconfig $HOME/.gitconfig
-cp -r myemacs $HOME/.myemacs
+mkdir -p       $HOME/.emacs.d
+cp emacs       $HOME/.emacs
+cp myinit.el   $HOME/.emacs.d/
+cp myinit.org  $HOME/.emacs.d/
+cp tmux.conf   $HOME/.tmux.conf
+cp gitconfig   $HOME/.gitconfig
+cp bashrc_user $HOME/.bashrc_user
 
 # make temp directory for emacs backup
 (cd $HOME && mkdir .emacsbackup)
+
+# Add custom bashrc
+echo "source ~/.bashrc_user" >> ~/.bashrc
